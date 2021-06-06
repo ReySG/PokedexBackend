282f5a6242ee4a684bac3c08a5ada15f431c86d7
ReySg <reinaldox99@gmail.com>
2021-06-06 16:36:11 -0400

    comment added

--- PokeApi/src/main/java/com/springboot/pokeapi/PokeApi/ResourceServerConfig.java
+++ PokeApi/src/main/java/com/springboot/pokeapi/PokeApi/ResourceServerConfig.java
@@ -14,8 +14,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter  {
 	
-//Configuracion para evitar problemas con el cors	
-	
+//Configuracion para evitar problemas con el cors
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
--- PokeApi/src/main/java/com/springboot/pokeapi/PokeApi/controllers/PokeApiController.java
+++ PokeApi/src/main/java/com/springboot/pokeapi/PokeApi/controllers/PokeApiController.java
@@ -1,12 +1,7 @@
 package com.springboot.pokeapi.PokeApi.controllers;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
